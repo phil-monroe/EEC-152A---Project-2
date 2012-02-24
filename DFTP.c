73a82261b81463c4e506bd2666d6024522c909c1
Phil Monroe <phil@philmonroe.com>
2012-02-24 13:09:24 -0800

    Added code for socket connections. Pretty much finished...

--- DFTP.c
+++ DFTP.c
@@ -1,43 +1,56 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 
+// Defines --------------------------------------------------------------------
+#define BUF_SIZE 1024
 
 // Pre-declarations -----------------------------------------------------------
 void init(char**, char**, int, char**);
-void print_header(char*, char*);
+void print_header(char*, char*, char*);
 void get_request(char**, size_t*);
 void print_help_message();
-void DFTP_request(char*);
+void DFTP_request(char*, struct addrinfo*);
+void get_ip(struct addrinfo*, char*);
+struct addrinfo* get_addrinfo(struct addrinfo* , char* , char* );
+
 
 
 // main -----------------------------------------------------------------------
 int main(int argc, char* argv[]){
 	// Initialize variables
-	char *host, *port;
-	size_t req_size = 256;
+	char *host, *port, ipstr[INET6_ADDRSTRLEN];
+	size_t req_size = BUF_SIZE;
 	char* request = (char*) malloc(req_size * sizeof(char));
+	
 	init(&host, &port, argc, argv);
 	
-	// TODO - Calculate IP
+	// Get IP from DNS
+  struct addrinfo hints, *res;
+	res = get_addrinfo(&hints, host, port);
+	get_ip(res, ipstr);
 	
-
-	print_header(host, port);
-	
+	// Print Header and start console
+	print_header(host, ipstr, port);
 	while(1){
 		get_request(&request, &req_size);
 
-		if(strcmp(request, "quit") == 0){
+		if(strcmp(request, "quit\n") == 0){
 			break;
-		}else if(strcmp(request, "help") == 0){
+		}else if(strcmp(request, "help\n") == 0){
 			print_help_message();
 		}else{
-			DFTP_request(request);
+			DFTP_request(request, res);
 		}
 	}
 	
 	puts("Goodbye.");
-	free(request);
+  freeaddrinfo(res); 	// free the results list
+	free(request);			// free request buffer
 	return 0;
 }
 
@@ -59,9 +72,9 @@ void init(char** host, char** port, int argc, char* argv[]){
 
 
 // print_header ---------------------------------------------------------------
-void print_header(char* host, char* port){
+void print_header(char* host, char* ip, char* port){
 	printf("Welcome to the simple DFTP client.\n");
-	printf("Connecting to %s(Calc IP) on port %s.\n", host, port);
+	printf("Connecting to %s(%s) on port %s.\n", host, ip, port);
 }
 
 
@@ -69,8 +82,6 @@ void print_header(char* host, char* port){
 void get_request(char** request, size_t* size){
 	printf("command > ");
 	getline(request, size, stdin);
-	char* newline = strchr(*request, '\n');
-	*newline = '\0';
 }
 
 
@@ -85,7 +96,69 @@ void print_help_message(){
 
 
 // DFTP_request ---------------------------------------------------------------
-void DFTP_request(char* request){
-	// TODO - Connect to server
-	puts(request);
+void DFTP_request(char* send_buf, struct addrinfo* server){
+	char recv_buf[BUF_SIZE];
+	int status;
+	int n_bytes;
+
+	// Create the socket
+	int sockfd = socket(server->ai_family, server->ai_socktype, server->ai_protocol);
+
+	// Connect
+	status = connect(sockfd, server->ai_addr, server->ai_addrlen);
+	if(status < 0){
+		fprintf(stderr, "A connection error occured.");
+		return;
+	}
+
+	// SEND!
+	n_bytes = send(sockfd, send_buf, strlen(send_buf), 0);
+
+	// Keep recieving data til n_bytes recieved == 0
+	while((n_bytes = recv(sockfd, recv_buf, BUF_SIZE, 0)) > 0){
+		recv_buf[n_bytes]='\0'; 			// Be sure to null terminate the string
+		printf("%s", recv_buf);
+	}
+	
+	// Close socket
+	close(sockfd);
 }
+
+
+// get_ip ---------------------------------------------------------------------
+void get_ip(struct addrinfo* res, char* ipstr){
+  void *addr;
+  char *ipver;
+
+  // get the pointer to the address itself,
+  // different fields in IPv4 and IPv6:
+  if (res->ai_family == AF_INET) { // IPv4
+      struct sockaddr_in *ipv4 = (struct sockaddr_in *)res->ai_addr;
+      addr = &(ipv4->sin_addr);
+      ipver = "IPv4";
+  } else { // IPv6
+      struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)res->ai_addr;
+      addr = &(ipv6->sin6_addr);
+      ipver = "IPv6";
+  }
+
+  // convert the IP to a string
+  inet_ntop(res->ai_family, addr, ipstr, INET6_ADDRSTRLEN);
+}
+
+
+// get_addrinfo ---------------------------------------------------------------
+struct addrinfo* get_addrinfo(struct addrinfo* hints, char* host, char* port){
+	int status;
+	struct addrinfo* res;
+  memset(hints, 0, sizeof(struct addrinfo));
+  hints->ai_family = AF_UNSPEC; // AF_INET or AF_INET6 to force version
+  hints->ai_socktype = SOCK_STREAM;
+
+  if ((status = getaddrinfo(host, port, hints, &res)) != 0) {
+      fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+      exit(2);
+  }
+	return res;
+}
+
